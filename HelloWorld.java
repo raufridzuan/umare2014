64715f27f2f4adf3bf4e596619e6b3935a54d298
Rauf Ridzuan <rauf.ridzuan@gmail.com>
2014-12-21 09:22:59 +0800

    straight print

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+class HelloWorld 
+{
+  public static void main(String[] args) 
+  {
+    // Objective:
+    //   Basic data disassembly of same string output with different techniques.
+
+    // umare@umare:~/codes/umare2014$ javac HelloWorld.java 
+    // umare@umare:~/codes/umare2014$ java HelloWorld 
+    // Hello world! --from UMARE 2014
+
+    //01 - Straight print    
+    System.out.println("Hello world! --from UMARE 2014"); 
+    
+  }
+}
